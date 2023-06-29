07158ceeeef0daeea152c150642498bf4ba8d4a1
Mauricio Jackson <mauricioj.fragoso@msn.com>
2023-06-29 17:14:34 -0300

    switch

---
+++ JavaScript2/exemplo-switch.js
@@ -0,0 +1,13 @@
+let letra = `a`;
+
+switch(letra) {
+    case `a`:
+    case `e`:
+    case `i`:
+    case `o`:
+    case `u`:
+        alert(`È uma vogal`);
+        break;
+    default:
+        alert(`não é uma vogal`)
+}
